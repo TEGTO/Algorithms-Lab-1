cbd4e4724c2abc5b29301a1b9504d22f67fff98e
shtrih2k4 <andreyzhevagin@gmail.com>
2022-10-31 18:26:55 +0200

    Gauss_Method_Main

--- main.cpp
+++ main.cpp
@@ -1,26 +1,13 @@
 #include <iostream>
 #include"algorithms.h"
 using namespace std;
-
-
-
-
-
-
-
-
-
-
-
-
 int main()
 {
 	setlocale(LC_ALL, "Rus");
-	size_t size = 4;
+	int size = 4;
 	double** A = new double* [size];
 	for (int i = 0; i < size; i++)
 		A[i] = new double[size];
-	//матриця для прикладу
 	A[0][0] = 6.0;
 	A[0][1] = 3.0;
 	A[0][2] = 8.0;
@@ -37,27 +24,8 @@ int main()
 	A[3][1] = 2.0;
 	A[3][2] = 3.0;
 	A[3][3] = 1.0;
-	//алгоритми обчислення оберненої матриці 
-	int type;
-	cout << "Алгоритми: " << endl;
-	cout << "3.LU алгоритм" << endl;
-	cout << "4.Метод Ньютона" << endl;
-	cout << "\nВиберiть алгоритм для обчислення: " << endl;
-	cin >> type;
-	cout << endl;
-	switch (type)
-	{
-	case 3:
-		LU::PshonovskiyMain(A, size); //Задача №3 LU алгоритм
-		break;
-	case 4:
-		Newtons_method::VolkovMain(A, size); //Задача №4 метод Ньютона алгоритм
-		break;
-	default:
-		break;
-	}
-	
-	
+	Gauss_Zhordan::Zhevahin_Main(A, size, size);
+
 
 	return 0;
 }
