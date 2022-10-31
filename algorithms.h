00e754a54a2aeb750e1cd8e79a2cdb97dd0919e6
TEGTO <90476119+TEGTO@users.noreply.github.com>
2022-10-31 20:49:58 +0200

    i hope everything is okay

--- algorithms.h
+++ algorithms.h
@@ -1,4 +1,15 @@
 #pragma once
+#pragma once
+
+namespace LU
+{
+	template <typename T>//створення шаблону
+	void PshonovskiyMain(T** A, int size);
+}
+namespace  Newtons_method
+{
+	void VolkovMain(double** A, int n);
+}
 namespace Gauss_Zhordan {
 	template <typename T>
 	void Zhevahin_Main(T** matrix, int size1, int size2);
